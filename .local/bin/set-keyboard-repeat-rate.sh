0532fe5562e093026b70bf8b7c80c4942e2f198f
Jeremy Villa <3289551+villapx@users.noreply.github.com>
2023-07-19 20:54:12 -0400

    Set keyboard repeat rate in GNOME/Wayland

--- .local/bin/set-keyboard-repeat-rate.sh
+++ .local/bin/set-keyboard-repeat-rate.sh
@@ -1,2 +1,23 @@
 #! /usr/bin/env bash
-xset r rate 200 35
+
+delay=200
+rate=35
+
+set -e
+
+if [[ -v "WAYLAND_DISPLAY" ]]; then
+    # wayland
+
+    if [[ "$XDG_CURRENT_DESKTOP" =~ "GNOME" ]]; then
+        set -x
+        gsettings set org.gnome.desktop.peripherals.keyboard repeat             true
+        gsettings set org.gnome.desktop.peripherals.keyboard delay              "$delay"
+        gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval    "$rate"
+    else
+        echo "Detected non-GNOME Wayland environment; need to figure out how to set this"
+        exit 1
+    fi
+else
+    # Xorg
+    xset r rate "$delay" "$rate"
+fi
