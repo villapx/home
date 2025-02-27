92dff59613c91b8905a893f24ab8435f8c3c4b48
Jeremy Villa <3289551+villapx@users.noreply.github.com>
2025-02-27 02:09:30 +0000

    Enable hidden files by default in telescope's 'find_files' picker

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -310,7 +310,7 @@ vim.keymap.set("n", "<Leader>t", nvimtree.tree.toggle, { noremap = true })
 vim.keymap.set("n", "<Leader>T", function() nvimtree.tree.find_file({ open = true, focus = true, }); end, { noremap = true })
 
 local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<Leader>ff", telescope.find_files, { noremap = true })
+vim.keymap.set("n", "<Leader>ff", function() telescope.find_files({hidden = true}); end, { noremap = true })
 vim.keymap.set("n", "<Leader>fg", telescope.live_grep, { noremap = true })
 vim.keymap.set("n", "<Leader>fo", telescope.buffers, { noremap = true })
 vim.keymap.set("n", "<Leader>fb", telescope.git_branches, { noremap = true })
