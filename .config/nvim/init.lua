cc61bf24bbf147c1f737e70ae8b695e667ca2b40
Jeremy Villa <3289551+villapx@users.noreply.github.com>
2026-01-06 20:44:52 +0000

    Update treesitter to track the 'main' branch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -134,6 +134,7 @@ require("lazy").setup({
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
